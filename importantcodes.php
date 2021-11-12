643f3a1e14af08e0d6befcba2fd0de9188fc0c7b
Mihir Patel <mihirp009@gmail.com>
2021-11-12 18:38:38 -0500

    Update importantcodes.php

--- importantcodes.php
+++ importantcodes.php
@@ -7,3 +7,8 @@ if (isset($_GET['writeyourreviewid'])) {
     $yourpostid = $_GET['writeyourreviewid'];
     comment_form($args = array(), $post_id = $yourpostid);
 }
+
+// Use of dynamic address in map with php variable
+$yourAddress = "Example Address or you can fetch address from backend options";
+?>
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.it/maps?q=<?php echo $yourAddress; ?>&output=embed"></iframe>
