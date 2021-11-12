9acddaadb448b8d9559d3ae60ee87fb96a9647d5
Mihir Patel <mihirp009@gmail.com>
2021-11-12 14:37:07 -0500

    Update importantcodes.php

--- importantcodes.php
+++ importantcodes.php
@@ -1,3 +1,9 @@
 <?php
 // Remove conflicts between ACF plugin and Wordpress Default Meta fields
 add_filter('acf/settings/remove_wp_meta_box', '__return_false');
+
+// If you want to create separate template for comment form. Pass post ID in URL and get that id on desired template 
+if (isset($_GET['writeyourreviewid'])) {
+    $pricexpostid = $_GET['writeyourreviewid'];
+    comment_form($args = array(), $post_id = $pricexpostid);
+}
