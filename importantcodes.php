a2fa4bd6bdadd7ecbb64264bb92aa29ab89da186
Mihir Patel <mihirp009@gmail.com>
2021-11-12 14:38:01 -0500

    Update importantcodes.php

--- importantcodes.php
+++ importantcodes.php
@@ -4,6 +4,6 @@ add_filter('acf/settings/remove_wp_meta_box', '__return_false');
 
 // If you want to create separate template for comment form. Pass post ID in URL and get that id on desired template 
 if (isset($_GET['writeyourreviewid'])) {
-    $pricexpostid = $_GET['writeyourreviewid'];
-    comment_form($args = array(), $post_id = $pricexpostid);
+    $yourpostid = $_GET['writeyourreviewid'];
+    comment_form($args = array(), $post_id = $yourpostid);
 }
